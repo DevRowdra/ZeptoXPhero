96279360d9f164ebb3c6240186203251515cd288
DevRowdra <121636096+DevRowdra@users.noreply.github.com>
2023-08-18 11:51:37 +0600

    php variable try

---
+++ 18.8.023 Php/Variables.php
@@ -0,0 +1,26 @@
+<?php
+$name='Rowdra';
+$age=124;
+// echo "my name is $name and age is $age  ";
+// ! php variable are cases sensitive 
+$nameE='Dhruba';
+// echo $nameE;
+// ! there is two types of variables in php 
+
+$fNumber=3.1416;
+// echo $fNumber;
+$boolean=true;
+$boolean2=false;
+echo $boolean2;
+
+// constant variables
+define('pi',1223.3);
+// echo  pi;
+define('_name','Rowdra');
+
+// echo _name;
+// no need to use $ sine for constant variables 
+define('bro','Sahara',) ;
+
+echo bro;
+
